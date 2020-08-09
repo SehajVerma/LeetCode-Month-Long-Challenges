c4b4c800988ab5b9b68d0f3ab6ea088ccfcd0b4f
Sehaj Verma <vermasehaj98@gmail.com>
2020-08-09 16:26:17 +0530

    Create Aug 8th ( Subarray sum k).cpp

---
+++ August Challenge/Aug 8th ( Subarray sum k).cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int c=0;
+public:
+    void solve(TreeNode* root,int sum,int temp,unordered_map<int,int>&mp)
+    {
+        if(root==NULL)
+            return;
+        temp+=root->val;
+        if(mp.find(temp-sum)!=mp.end())
+        {
+            c+=mp[temp-sum];
+        }
+        mp[temp]++;
+        solve(root->left,sum,temp,mp);
+        solve(root->right,sum,temp,mp);
+        mp[temp]--;
+    }
+    int pathSum(TreeNode* root, int sum) {
+        unordered_map<int,int>mp;
+        mp.insert({0,1});
+        solve(root,sum,0,mp);
+        return c;
+    }
+};
