f4d942af3179923f50be6f446817b51f651fe72e
Sehaj Verma <vermasehaj98@gmail.com>
2020-08-13 11:28:34 +0530

    Create Aug 12th (Pascal Triangle).cpp

---
+++ August Challenge/Aug 12th (Pascal Triangle).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int>v;
+        v.push_back(1);
+        for(int i=0;i<rowIndex;i++)
+        {
+            for(int j=v.size()-1;j>0;j--)
+            {
+                v[j] = v[j]+v[j-1];
+            }
+            v.push_back(1);
+        }
+        return v;
+    }
+};
